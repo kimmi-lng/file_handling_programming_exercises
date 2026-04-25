5a39dc4c1913c9375c18d2dd0d31c8a3b4e9cae0
Kim Russel <kimrussel0718@gmail.com>
2026-04-25 22:08:01 +0800

    created two fuction, first is to look or read the entire txt file and second is to categorize if the number is an odd or even integer.

--- reads_txt_file_p1/reads_txt_file.py
+++ reads_txt_file_p1/reads_txt_file.py
@@ -1,3 +1,11 @@
 class ExtractNumbers:
     def __init__(self, reads_txt_file = "numbers.txt"):
         self.reads_txt_file = reads_txt_file
+
+    def read_file(self):
+        with open(self.reads_txt_file, "r") as file:
+            content = [line.rstrip('\n') for line in file.readlines()]
+        return content
+
+    def find_even_odd(self):
+        content = self.read_file()
