f9a7fa9413ec9391c3ecea103cb5f7a398620e58
Kim Russel <kimrussel0718@gmail.com>
2026-04-26 23:45:14 +0800

    added a class named SortIntegers and then fill the parameter with defaults.

--- squared_cubed_integers_p4/squared_cubed_integers.py
+++ squared_cubed_integers_p4/squared_cubed_integers.py
@@ -1,1 +1,11 @@
-ECHO is on.
+class SortIntegers:
+    def __init__(self, integers = "integers.txt", double = "double.txt", triple = "triple.txt"):
+        self.integers = integers
+        self.double = double
+        self.triple = triple
+    
+    def read_integers(self):
+        with open(self.integers, "r") as file:
+            content = [line.rstrip('\n') for line in file.readlines()]
+        return content
+    
