e483e005134e3ee9a38728eb58966d59b10ed659
Kim Russel <kimrussel0718@gmail.com>
2026-04-26 16:35:18 +0800

    after adding the class i now proceed to define the fuctions or the method, read_file() to open and read the existing txt file and return its data, find_top_student() here we first set the values for gwa and the name of the student so that we can compare using if else statement then return their value. i also created another python file named look_highes_gwa_v2.py so that i can instantiate the method.

--- look_highest_gwa_p2/look_highest_gwa.py
+++ look_highest_gwa_p2/look_highest_gwa.py
@@ -1,3 +1,24 @@
 class StudentsGwa:
     def __init__(self, names_gwa_file = "student_names_with_gwa.txt"):
         self.names_gwa_file = names_gwa_file
+    
+    def read_file(self):
+        with open(self.names_gwa_file, "r") as file:
+            content = [line.rstrip('\n') for line in file.readlines()]
+        return content
+    
+    def find_top_student(self):
+        highest_gwa = -1
+        top_student = ""
+
+        content = self.read_file()
+
+        for line in content:
+            name, gwa = line.strip().split(",")
+            gwa = float(gwa)
+
+            if gwa > highest_gwa:
+                highest_gwa = gwa
+                top_student = name
+
+        return top_student, highest_gwa
