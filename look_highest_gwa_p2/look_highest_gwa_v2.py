bd40f477b12a394b0ca52bbdc6ca8128c5d1b4c7
Kim Russel <kimrussel0718@gmail.com>
2026-04-26 16:58:00 +0800

    rechecked the directory

---
+++ look_highest_gwa_p2/look_highest_gwa_v2.py
@@ -0,0 +1,8 @@
+from look_highest_gwa import StudentsGwa
+
+checker = StudentsGwa()
+top_name, top_gwa = checker.find_top_student()
+
+# Output the result
+print(f"Top Student: {top_name}")
+print(f"GWA: {top_gwa}")
