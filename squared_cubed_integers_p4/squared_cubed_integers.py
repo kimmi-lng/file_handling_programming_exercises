6a35fd339f0c4497fd9e91fa88c03dee34910223
Kim Russel <kimrussel0718@gmail.com>
2026-04-27 00:38:16 +0800

    change the parameter with much more precise directory cuz its making an error for not finding the txt files.

--- squared_cubed_integers_p4/squared_cubed_integers.py
+++ squared_cubed_integers_p4/squared_cubed_integers.py
@@ -1,5 +1,8 @@
 class SortIntegers:
-    def __init__(self, integers = "integers.txt", double = "double.txt", triple = "triple.txt"):
+    def __init__(self, 
+             integers = "squared_cubed_integers_p4/integers.txt", 
+             double = "squared_cubed_integers_p4/double.txt", 
+             triple = "squared_cubed_integers_p4/triple.txt"):
         self.integers = integers
         self.double = double
         self.triple = triple
@@ -13,9 +16,10 @@ class SortIntegers:
         content = self.read_integers()
         with open(self.double, "w") as double_file, open(self.triple, "w") as triple_file:
             for line in content:
-                numbers = int(line)
-                if numbers % 2 == 0:
-                    double_file.write(f"{numbers ** 2}\n")
-                else:
-                    triple_file.write(f"{numbers ** 3}\n")
+                if line.strip():
+                    numbers = int(line)
+                    if numbers % 2 == 0:
+                        double_file.write(f"{numbers ** 2}\n")
+                    else:
+                        triple_file.write(f"{numbers ** 3}\n")
     
